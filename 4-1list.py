681942137bf63bad0fe3408b02bd236455a24355
aqutw <aquarian.ex@gmail.com>
2015-03-20 19:36:39 +0800

    Add 4-1~4-4

---
+++ 4-1list.py
@@ -0,0 +1,5 @@
+empty_list = []
+print empty_list
+
+L = ['Adam', 95.5, 'Lisa', 85, 'Bart', 59]
+print L
---
+++ 4-4append_insert.py
@@ -0,0 +1,5 @@
+L = ['Adam', 'Lisa', 'Bart']
+L.insert(2, 'Paul1')
+L.insert(0, 'Paul2')
+L.append('Paul3')
+print L
