096ca21753892082646406cd912bd279d0820d69
aqutw <aquarian.ex@gmail.com>
2015-03-20 19:24:17 +0800

    Add 3-6raw.py

---
+++ 3-6raw.py
@@ -0,0 +1,22 @@
+print r'\(~_~)/ \(~_~)/' # r + single-quote = cannot include " and '
+
+print r'''
+line1
+line2
+line3''' # r + 3*single-quote = can include " ' \n
+
+print 'Line 1\nLine 2\nLine 3'
+
+
+
+print r'''Python is created by "Guido".
+It is free and easy to learn.
+Let's start learn Python in imooc!'''
+
+
+
+
+# do Task
+print '\"To be, or not to be\": that is the question.\nWhether it\'s nobler in the mind to suffer.'
+print r'''"To be, or not to be": that is the question.
+Whether it's nobler in the mind to suffer.'''
