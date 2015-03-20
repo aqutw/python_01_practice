f7e450d4b438421d1adbd9f711646a6f19292ab8
aqutw <aquarian.ex@gmail.com>
2015-03-20 19:42:57 +0800

    Add 4-5 4-6

---
+++ 4-5pop.py
@@ -0,0 +1,5 @@
+L = ['Adam', 'Lisa', 'Paul', 'Bart']
+L.pop()
+print L
+L.pop(1)
+print L
---
+++ 4-6.py
@@ -0,0 +1,13 @@
+a = ['Adam', 'Lisa', 'Bart']
+a[2] = 'Pual'
+print a
+
+a[-1] = 'Paul2'
+print a
+
+
+L = ['Adam', 'Lisa', 'Bart']
+L[-1] = 'Adam'
+L[0] = 'Bart'
+print L
+
