ecb750eb1dcd86950355841ffde10aa935920561
aqutw <aquarian.ex@gmail.com>
2015-03-22 11:40:46 +0800

    [part2] Add 4-2 comment

--- 02advanced/4-2define_a_class.py
+++ 02advanced/4-2define_a_class.py
@@ -1,4 +1,4 @@
-class Person(object): #<---the parameter naming object is...?
+class Person(object): #<---the parameter naming object is...? In Python, All classes will be under object. #This syntax is like (Ruby) Person < object  OR  (PHP)  Person extends object
     pass
 xiaoming = Person()
 xiaohong = Person()
